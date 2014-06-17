1bbe7c31320da5fe3a307fd534939fa558562b80
psep <psep@debian.cl>
2014-06-17 17:04:03 -0400

    mod class

--- AES/src/cl/psep/aes/demo/main/Test.java
+++ AES/src/cl/psep/aes/demo/main/Test.java
@@ -13,30 +13,5 @@ public class Test {
 		System.out.println("Encrypted string: " + d.encrypt("Hello"));
 		String encryptedText = d.encrypt("Hello");
 		System.out.println("Decrypted string: " + d.decrypt(encryptedText));
-		
-		Prueba prueba = new Prueba();
-		prueba.uno = 1;
-		prueba.dos = 2;
-		prueba.tres= "tres !!!";
-		
-		ArrayList<Object> lista = new ArrayList<Object>();
-		lista.add(prueba);
-		lista.add("Esto es un mensaje");
-		lista.add(123);
-		
-		Iterator<Object> iter = lista.iterator();
-		
-		while (iter.hasNext()) {
-			Object object = (Object) iter.next();
-			Class<? extends Object> clase = object.getClass();
-			
-			System.out.println(clase.getName());
-		}
 	}
-}
-
-class Prueba {
-	public int uno;
-	public Integer dos;
-	public String tres;
 }
