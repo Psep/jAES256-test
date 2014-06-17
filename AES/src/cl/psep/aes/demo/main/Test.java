194cfe9f82f3a22effa74afde1d8330e5087f551
psep <psep@debian.cl>
2014-06-17 17:09:20 -0400

    mod test

--- AES/src/cl/psep/aes/demo/main/Test.java
+++ AES/src/cl/psep/aes/demo/main/Test.java
@@ -10,8 +10,8 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		AESDemo d = new AESDemo();
 
-		System.out.println("Encrypted string: " + d.encrypt("Hello"));
 		String encryptedText = d.encrypt("Hello");
+		System.out.println("Encrypted string: " + encryptedText);
 		System.out.println("Decrypted string: " + d.decrypt(encryptedText));
 	}
 }
